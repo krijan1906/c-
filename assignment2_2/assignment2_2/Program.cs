7196cc196737cd15e8500109a67bcd407c566568
Krijan Maharjan <krijanmaharjan31@gmail.com>
2026-07-17 09:17:44 +0545

    first commit

---
+++ assignment2_2/assignment2_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class student
+{
+    string name;
+    int id;
+    public student()
+    {
+        name = "krijan";
+        id = 3;
+    }
+    public student(string name,int id)
+    {
+        this.name = name;
+        this.id = id;
+
+    }
+    public student(string name)
+    {
+        this.name = name;
+        this.id = 5;
+    }
+    public void display()
+    {
+        Console.WriteLine("name :" + name);
+        Console.WriteLine("id:" + id);
+
+    }
+
+
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        student s1 = new student();
+        s1.display();
+        student s2 = new student("krijan");
+        s2.display();
+        student s3 = new student("bibek", 4);
+        s3.display();
+    }
+}
