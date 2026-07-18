476999d0adc25114222f91e8b7dc9c74e9508393
Krijan Maharjan <krijanmaharjan31@gmail.com>
2026-07-18 22:30:28 +0545

    third commit

---
+++ c#lab1/question15/question15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public partial class Student
+{
+    public void DisplayName()
+    {
+        Console.WriteLine("Name : Ram");
+    }
+}
+
+public partial class Student
+{
+    public void DisplayAge()
+    {
+        Console.WriteLine("Age : 20");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+
+        s.DisplayName();
+        s.DisplayAge();
+    }
+}
